e41710e75beac08a88ce687b07787c6b609b5265
flur01 <skarik2001@gmail.com>
2020-04-10 15:17:14 +0300

    Aded new design

--- app/view.py
+++ app/view.py
@@ -52,24 +52,28 @@ def add_user():
         new_surname = form.surname.data
         new_email = form.email.data  
         new_birthday = form.birthday.data
+        user = db.session.query(User).filter(User.email == new_email).first()
+        if not user:
+            if form.gender.data == "u":
+                new_gender = None
+            else:
+                new_gender = bool(int(form.gender.data))
 
-        if form.gender.data == "u":
-            new_gender = None
+            if new_birthday == date.today():
+                new_birthday = None
+
+            new_user = User(name=new_name, surname=new_surname, email=new_email, gender=new_gender, birthday=new_birthday)
+            
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect('/login/')
+            except:
+                flash("Error with adding new user.")
+                return render_template('adduser.html', form=form, message="") 
         else:
-            new_gender = bool(int(form.gender.data))
-
-        if new_birthday == date.today():
-            new_birthday = None
-
-        new_user = User(name=new_name, surname=new_surname, email=new_email, gender=new_gender, birthday=new_birthday)
-        
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect('/login/')
-        except:
             flash("Error with adding new user.")
-            return render_template('adduser.html', form=form, message="") 
+            return render_template('adduser.html', form=form, message="")
     
     return render_template('adduser.html', form=form)   
 
@@ -79,12 +83,10 @@ def add_user():
 def feedback():
     form = AddFeedback()
     if request.method == 'POST' and form.validate_on_submit(): 
-        try:
             user = db.session.query(User).filter(current_user.email == form.email.data and current_user.name == form.name.data).first()
             if user:
-                user_id = user.id
                 content = form.feedback.data
-                new_comment = Comment(user_id=user_id,content=content)
+                new_comment = Comment(user_id=current_user.id,content=content)
                 try:
                     db.session.add(new_comment)
                     db.session.commit()
@@ -92,10 +94,9 @@ def feedback():
                 except:
                     flash("Error with adding new comment")
                     return render_template('addfeedback.html', form=form)
-        except:
-            flash("Invalid name/email")
-            return render_template('addfeedback.html', form=form)
-    flash("Error")
+            else:
+                flash("Invalid name/email or they aren't yours ")
+                return render_template('addfeedback.html', form=form)
     return render_template('addfeedback.html', form=form)
 
 
@@ -104,6 +105,8 @@ def feedback():
 def comment():
     comments = db.session.query(User, Comment).outerjoin(Comment, User.id == Comment.user_id).all()
     return render_template('comments.html', comments=comments)
+
+
    
 
 @app.route('/delete/<int:id>')
@@ -130,7 +133,7 @@ def user_info():
 @login_required
 def index():
     params ={
-        'spider_name': 'test',
+        'spider_name': 'weather_spider',
         'start_requests': True,
     }
     response = requests.get('http://localhost:9080/crawl.json',params)
@@ -145,7 +148,7 @@ def index():
     time = [i for i in range(0,22,3) ]
 
     return render_template('index.html', 
-    data=data, l_data=len(data[0][2]),#[2]
+    data=data, l_data=len(data[0][2]),
     time=time)
 
 
